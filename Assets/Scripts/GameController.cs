f638e90da658d3423c6123f527f53c1126cc9460
Eduardo Muro <eduardomurogiurfa@gmail.com>
2022-07-07 13:24:01 -0500

    se creo el script crear jugadores, como el del profe (pero sigue vacio)

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
@@ -11,13 +12,42 @@ public class GameController : MonoBehaviour
 
     [SerializeField] private AudioSource tuParlante;
 
+    [SerializeField] private TextMeshProUGUI RelojCounterHUD;
+    [SerializeField] private int RelojGame = 400;
+    [HideInInspector] public bool isGameOver = false;
+
     //[SerializeField] private TextMeshProUGUI txtScore;
     //int playerScore = 0;
     // Start is called before the first frame update
     void Start()
     {
         //txtScore.text = " x" + playerScore;
+        RelojCounterHUD.text = "" + RelojGame;
+        InvokeRepeating("ReduceValue", 0, 1);
     }
+    public void ReduceValue()
+    {
+        RelojGame--;
+        RelojCounterHUD.text = "" + RelojGame;
+    }
+    private void Update()
+    {
+        if (RelojGame <= 0 && isGameOver == false)
+        {
+            isGameOver = true;
+            RelojGame = 0;
+            print("Fin del juego");
+            //StartCoroutine(gameOver("GameOverJ"));
+            //SceneManager.LoadScene("GameOverJ");
+        }
+    }
+    //IEnumerator gameOver(string name)
+    //{
+    //    print("Se inicio Co-rutina Game Over");
+    //    yield return new WaitForSeconds(1);
+    //    SceneManager.LoadScene(name);
+    //}
+
     public void PlaySound(int index)
     {
         switch(index)
@@ -34,6 +64,7 @@ public class GameController : MonoBehaviour
 
         }
     }
+
     //public void UpdateScore()
     //{
     //    playerScore++;
