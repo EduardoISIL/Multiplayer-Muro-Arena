271c6e2c3bbf78fe3b3e83883c5d95b89a7abb91
markoaar <markoarenas@gmail.com>
2022-07-12 02:03:58 -0500

    Lobby & Menu
    Se dio consistencia a ambos scripts, y ahora el LAUNCHER se encarga de generar al player en el nivel 1. Se corrigieron botones y cambios de escena, junto al PUN.

--- Assets/Scripts/Launcher.cs
+++ Assets/Scripts/Launcher.cs
@@ -7,18 +7,33 @@ using UnityEngine.SceneManagement;
 
 public class Launcher : MonoBehaviourPunCallbacks
 {
-    [SerializeField] private PhotonView playerPref1;
-    [SerializeField] private CameraFollower cam;
-    [SerializeField] private Transform SpawnDefault;
 
+    // Menu
     [SerializeField] private GameObject loadingPnl;
+    private bool menuState = false;
+    private bool playPressed = false;
+
+    // Lobby
     [SerializeField] private Sprite[] playerSkins;
     [SerializeField] private SpriteRenderer playerEdit;
     private int tempValue = 0;
     private int skinsCount = 0;
-    // Start is called before the first frame update
+    private bool lobbyState = false;
+    //private bool levelState = false; HABILITAR CUANDO SE PUEDA REGRESAR AL MENU
+    private bool startPressed = false;
+    private bool backPressed = false;
+    private bool isBack = false;
+    private GameObject backBtn = null;
+    [SerializeField] private RuntimeAnimatorController[] animController;
 
-    public static Launcher launcherStatic;//comparte esta variable con todas las escenas
+    // Nivel 1
+    private bool inGame = false;
+    [SerializeField] private PhotonView playerPref1;
+    [SerializeField] private CameraFollower cam;
+    [SerializeField] private Transform SpawnDefault;
+
+    // Propiedad DON'T DESTROY ON LOAD
+    public static Launcher launcherStatic; //comparte esta variable con todas las escenas
 
     private void Awake()
     {
@@ -30,89 +45,175 @@ public class Launcher : MonoBehaviourPunCallbacks
         launcherStatic = this;
         DontDestroyOnLoad(this);
     }
-
-    void Start()
-    {
-        PhotonNetwork.ConnectUsingSettings();
-        skinsCount = playerSkins.Length;
-        tempValue = 0;
-        print(tempValue);
-
-        if (GameObject.Find("PlayerEdit") != null)
-        {
-            playerEdit = GameObject.Find("PlayerEdit").GetComponent<SpriteRenderer>();
-        }
-
-        if (SceneManager.GetActiveScene().name == "2")
-        {
-            print("Se detecto la escena");
-            playerPref1 = GameObject.Find("MC 2").GetComponent<PhotonView>();
-            cam = GameObject.Find("Main Camera").GetComponent<CameraFollower>();
-            SpawnDefault = GameObject.Find("SpawnPosition").GetComponent<Transform>();
-            PhotonNetwork.JoinRandomOrCreateRoom();
-        }
-    }
+    //---------------------
 
     public override void OnConnectedToMaster()
     {
-        print("Conectado con exito al servidor");
+        print("Conectado con exito al servidor"); 
         if (loadingPnl != null)
         {
-            loadingPnl.SetActive(false);
+            loadingPnl.SetActive(false); // Al conectar con el servidor se retira la pantalla de LOADING...
         }
     }
 
-    public void Play()
+    private void Update()
     {
-        SceneManager.LoadScene(1);
+        switch (SceneManager.GetActiveScene().name)
+        {
+            case "Menu":
+
+                if (isBack == false && menuState == false)
+                {
+                    print("Menu Case");
+                    lobbyState = false;
+                    playPressed = false;
+                    menuState = true;
+                    PhotonNetwork.ConnectUsingSettings();
+
+                    skinsCount = playerSkins.Length;
+                    tempValue = 0;
+                }
+
+                if (isBack == true)
+                {
+                    lobbyState = false;
+                    playPressed = false;
+                    loadingPnl = GameObject.Find("LoadingPnl");
+                    if (loadingPnl != null)
+                    {
+                        loadingPnl.SetActive(false);
+                    }
+                    if (GameObject.Find("Play") != null)
+                    {
+                        GameObject.Find("Play").GetComponent<Button>().onClick.AddListener(Play);
+                    }
+                }
+
+                break;
+
+            case "Lobby":
+
+                if (lobbyState == false)
+                {
+                    print("Lobby Case");
+
+                    menuState = false;
+                    isBack = false; 
+                    backPressed = false;
+                    lobbyState = true;
+
+                    PhotonNetwork.JoinLobby();
+                    backBtn = GameObject.Find("BackBtn").gameObject;
+
+                    tempValue = 0;
+
+                    if (GameObject.Find("Left") != null)
+                    {
+                        GameObject.Find("Left").GetComponent<Button>().onClick.AddListener(LeftSkin);
+                    }
+
+                    if (GameObject.Find("Right") != null)
+                    {
+                        GameObject.Find("Right").GetComponent<Button>().onClick.AddListener(RightSkin);
+                    }
+
+                    if (GameObject.Find("StartBtn") != null)
+                    {
+                        GameObject.Find("StartBtn").GetComponent<Button>().onClick.AddListener(StartGame);
+                    }
+
+                    if (GameObject.Find("BackBtn") != null)
+                    {
+                        GameObject.Find("BackBtn").GetComponent<Button>().onClick.AddListener(BackMenu);
+                    }
+
+                    if (GameObject.Find("loadingTxt") != null)
+                    {
+                        GameObject.Find("loadingTxt").GetComponent<Text>().text = "";
+                    }
+
+                    if (GameObject.Find("PlayerEdit") != null)
+                    {
+                        playerEdit = GameObject.Find("PlayerEdit").GetComponent<SpriteRenderer>();
+                    }
+                }
+
+                if (playerEdit != null) playerEdit.sprite = playerSkins[tempValue];
+
+                break;
+
+            case "Nivel 1":
+
+                if (inGame == false)
+                {
+                    inGame = true;
+                    print("Nivel 1 Case");
+                    PhotonNetwork.JoinRandomOrCreateRoom();
+                    //levelState = true; HABILITAR CUANDO SE PUEDA REGRESAR AL MENU
+                    SpawnDefault = GameObject.Find("SpawnPosition").transform;
+                    cam = GameObject.Find("Main Camera").GetComponent<CameraFollower>();
+                    print("Se creo el Room");
+                }
+
+                break;
+        }
     }
 
-    public void StartGame()
-    {
-        SceneManager.LoadScene(2);
-    }
-
+    // Paso 5 - Al ingresar a la sala
     public override void OnJoinedRoom()
     {
         Debug.Log("Ingreso a sala exitosa");
-
+        print(tempValue);
+        
         if (playerPref1 != null && cam != null && SpawnDefault != null)
         {
             PhotonNetwork.Instantiate(playerPref1.name, SpawnDefault.position, Quaternion.identity);
+            playerPref1.GetComponent<Animator>().runtimeAnimatorController = animController[tempValue];
             LookAtForPLayer();
         }
     }
 
+    // Por DEFINIR
     public void LookAtForPLayer()
     {
         cam.LookOutForThePlayer();
     }
 
-    private void Update()
+    // Botón de Menú
+    public void Play()
     {
-        if (GameObject.Find("PlayerEdit") != null)
+        if (playPressed == false)
         {
-            playerEdit = GameObject.Find("PlayerEdit").GetComponent<SpriteRenderer>();
-        }
-
-        if (playerEdit != null) playerEdit.sprite = playerSkins[tempValue];
-
-        if (GameObject.Find("Left") != null)
-        {
-            GameObject.Find("Left").GetComponent<Button>().onClick.AddListener(LeftSkin);
-        }
-
-        if (GameObject.Find("Right") != null)
-        {
-            GameObject.Find("Right").GetComponent<Button>().onClick.AddListener(RightSkin);
-        }
-
-        if (GameObject.Find("StartBtn") != null)
-        {
-            GameObject.Find("StartBtn").GetComponent<Button>().onClick.AddListener(StartGame);
+            playPressed = true;
+            SceneManager.LoadScene(1);
         }
     }
 
+    // Botón de Lobby
+    public void StartGame()
+    {
+        if (startPressed == false)
+        {
+            GameObject.Find("loadingTxt").GetComponent<Text>().text = "Loading...";
+            startPressed = true;
+            backBtn.SetActive(false);
+            SceneManager.LoadScene(2);
+        }
+    }
+
+    public void BackMenu()
+    {
+        if (backPressed == false)
+        {
+            GameObject.Find("loadingTxt").GetComponent<Text>().text = "Loading...";
+            backPressed = true;
+            isBack = true;
+            PhotonNetwork.LeaveLobby();
+            SceneManager.LoadScene(0);
+        }
+    }
+
+    // Selector de Skin
     public void RightSkin()
     {
         if (tempValue < skinsCount - 1)
